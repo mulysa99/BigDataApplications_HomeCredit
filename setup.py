964bf2790ea11cbe9eb9595356c1f19744a21fba
alex6970 <alexandra.goncalves@efrei.net>
2021-11-16 16:49:03 +0100

    Pj initialization

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Course project in Big Data for Applications',
+    author='Goncalves_Nitschke_Keutcha',
+    license='MIT',
+)
